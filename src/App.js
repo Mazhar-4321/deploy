e2e56bbf19df87fec87b597e7d57ba61538eed47
Mazhar-4321 <syedmazharali742@gmail.com>
2023-05-30 11:34:48 +0530

    [Mazhar]ADDED: Project To Master Branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Counter } from './features/counter/Counter';
+import { CrudUsecase } from './crudUsecase/CrudUsecase';
+import SampleTabs from './MuiUsecase/MuiUsecase';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "rgb(0,0,0,1)",
+    },
+    secondary:{
+      main:'#FFFFFF'
+    }
+  },
+});
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+      <SampleTabs />
+      </ThemeProvider>
     </div>
   );
 }
